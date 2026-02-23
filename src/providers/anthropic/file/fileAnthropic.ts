e1387eb583a8b45303eff6fef8cfcc1983637b5e
victor.fuly <victor.fuly@cubos.io>
2026-02-23 11:27:32 -0300

    feat: add create file to anthropic

--- src/providers/anthropic/file/fileAnthropic.ts
+++ src/providers/anthropic/file/fileAnthropic.ts
@@ -1,13 +1,22 @@
+import type { FileOptions } from "../../../types/file.js";
 import { FileProviderBase } from "../../fileProviderBase.js";
 import type { AnthropicProvider } from "../index.js";
 
 export class FileAnthropic extends FileProviderBase<AnthropicProvider> {
-  create(): Promise<void> {
-    this.provider.client.files.upload({
-      file: new Blob(["Hello, world!"], { type: "text/plain" }),
-    });
-    throw new Error("Method not implemented.");
+  async create(file: File, options: FileOptions): Promise<void> {
+    this.checkFileSupport(file);
+
+    const request = {
+      file: file,
+    };
+
+    await this.provider.hooks.handleRequest(request);
+
+    const response = await this.provider.client.beta.files.upload(request);
+
+    await this.provider.hooks.handleResponse(request, response, options.metadata ?? {});
   }
+
   list(): Promise<void> {
     throw new Error("Method not implemented.");
   }
