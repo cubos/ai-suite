bb07213b310409a017a4d6f920ae75683029849d
victor.fuly <victor.fuly@cubos.io>
2026-02-23 15:45:19 -0300

    feat: add list files to gemini

--- src/providers/gemini/file/fileGemini.ts
+++ src/providers/gemini/file/fileGemini.ts
@@ -1,9 +1,16 @@
-import type { FileOptions, SuccessCreateFile } from "../../../types/file.js";
+import type { ListFilesParameters } from "@google/genai";
+import type {
+  CreateFileOptions,
+  FileResponse,
+  ListFileOptions,
+  SuccessCreateFile,
+  SuccessListFile,
+} from "../../../types/file.js";
 import { FileProviderBase } from "../../fileProviderBase.js";
 import type { GeminiProvider } from "../geminiProvider.js";
 
 export class FileGemini extends FileProviderBase<GeminiProvider> {
-  async create(file: File, options: FileOptions): Promise<SuccessCreateFile> {
+  async create(file: File, options: CreateFileOptions): Promise<SuccessCreateFile> {
     this.checkFileSupport(file);
 
     const request = {
@@ -28,8 +35,39 @@ export class FileGemini extends FileProviderBase<GeminiProvider> {
       expires_at: response.expirationTime ? Math.floor(new Date(response.expirationTime).getTime() / 1000) : undefined,
     };
   }
-  list(): Promise<void> {
-    throw new Error("Method not implemented.");
+  async list(options: ListFileOptions): Promise<SuccessListFile> {
+    const request: ListFilesParameters = {
+      config: {
+        pageSize: options.limit ?? 10,
+        pageToken: options.after,
+      },
+    };
+
+    await this.provider.hooks.handleRequest(request);
+
+    const response = await this.provider.client.files.list(request);
+
+    const files: FileResponse[] = [];
+
+    for await (const file of response) {
+      files.push({
+        id: file.name || "",
+        bytes: file.sizeBytes ? Number(file.sizeBytes) : 0,
+        created_at: file.createTime ? Math.floor(new Date(file.createTime!).getTime() / 1000) : 0,
+        filename: file.name || "",
+        object: "file",
+        expires_at: file.expirationTime ? Math.floor(new Date(file.expirationTime).getTime() / 1000) : undefined,
+      });
+    }
+
+    await this.provider.hooks.handleResponse(request, response, options.metadata ?? {});
+
+    return {
+      success: true,
+      model: this.provider.model,
+      content: files,
+      has_next_page: response.hasNextPage(),
+    };
   }
   retrieve(): Promise<void> {
     throw new Error("Method not implemented.");
